c32213b143b638958ec5630b0de2d863477c6e30
Raymond Kroon <raymond@k3n.nl>
2015-11-23 22:09:21 +0100

    refactor(tokenizer): None -> EOF, add namespace

--- libfancy/include/tokenizer.hpp
+++ libfancy/include/tokenizer.hpp
@@ -39,117 +39,123 @@ const std::set<char> delimiters = boost::assign::list_of('{')('}')('[')(']')('('
 
 typedef std::shared_ptr<std::string> shared_string;
 
-enum TokenType {
-  None, // initialised value;
-  Literal, RoundOpen, RoundClose, SquareOpen, SquareClose, CurlyOpen, CurlyClose,
-  SetOpen, FunctionOpen, Dispatch, String, Regex, Char
-};
+namespace token {
 
-const boost::unordered_map<TokenType, const char*> tokenTypeTranslations = boost::assign::map_list_of
-    (None, "None")(Literal, "LIT")(RoundOpen, "(")(RoundClose, ")")(SquareOpen,"[")(SquareClose, "]")
-    (CurlyOpen, "{")(CurlyClose, "}")(SetOpen,"#{")(FunctionOpen, "#(")(Dispatch, "#")(String, "STR")
-    (Regex, "#\"")(Char, "CH");
+  enum TokenType {
+    EndOfFile,
+    Literal, RoundOpen, RoundClose, SquareOpen, SquareClose, CurlyOpen, CurlyClose,
+    SetOpen, FunctionOpen, Dispatch, String, Regex, Char
+  };
 
-class Token {
-public:
-  Token(const Token &other) : type(other.type), value(std::string(other.value)), pos(position(other.pos)) {}
+  const boost::unordered_map<TokenType, const char *> tokenTypeTranslations = boost::assign::map_list_of
+      (EndOfFile, "EOF")(Literal, "LIT")(RoundOpen, "(")(RoundClose, ")")(SquareOpen, "[")(SquareClose, "]")
+      (CurlyOpen, "{")(CurlyClose, "}")(SetOpen, "#{")(FunctionOpen, "#(")(Dispatch, "#")(String, "STR")
+      (Regex, "#\"")(Char, "CH");
 
-  Token(Token&& other) : type(std::move(other.type)), value(std::move(other.value)), pos(std::move(other.pos)) {}
-  Token& operator=(Token&& other) {
-    std::swap(this->type, other.type);
-    std::swap(this->value, other.value);
-    std::swap(this->pos, other.pos);
+  class Token {
+  public:
+    Token(const Token &other) : type(other.type), value(std::string(other.value)), pos(position(other.pos)) { }
 
-    return *this;
-  }
+    Token(Token &&other) : type(std::move(other.type)), value(std::move(other.value)), pos(std::move(other.pos)) { }
+    Token &operator=(Token &&other) {
+      std::swap(this->type, other.type);
+      std::swap(this->value, other.value);
+      std::swap(this->pos, other.pos);
 
-  ~Token() {};
-
-  std::string DebugInfo() const {
-    if (value.empty()) {
-      return std::string(tokenTypeTranslations.at(type))
-             + " (" + std::to_string(std::get<0>(pos)) + ", "
-             + std::to_string(std::get<1>(pos)) + ")";
+      return *this;
     }
-    else {
-      return std::string(tokenTypeTranslations.at(type)) + " " + value
-             + " (" + std::to_string(std::get<0>(pos)) + ", "
-             + std::to_string(std::get<1>(pos)) + ")";
+
+    ~Token() { };
+
+    std::string DebugInfo() const {
+      if (value.empty()) {
+        return std::string(tokenTypeTranslations.at(type))
+               + " (" + std::to_string(std::get<0>(pos)) + ", "
+               + std::to_string(std::get<1>(pos)) + ")";
+      }
+      else {
+        return std::string(tokenTypeTranslations.at(type)) + " " + value
+               + " (" + std::to_string(std::get<0>(pos)) + ", "
+               + std::to_string(std::get<1>(pos)) + ")";
+      }
     }
+
+    bool operator==(const Token other) const;
+
+    TokenType type;
+    std::string value;
+    position pos;
+
+    static Token EndOfFile() {
+      return Token(TokenType::EndOfFile, "", std::make_tuple(-1, -1));
+    }
+
+    static Token Literal(std::string value, position pos) {
+      return Token(TokenType::Literal, value, pos);
+    }
+
+    static Token RoundOpen(position pos) {
+      return Token(TokenType::RoundOpen, "", pos);
+    }
+
+    static Token RoundClose(position pos) {
+      return Token(TokenType::RoundClose, "", pos);
+    }
+
+    static Token SquareOpen(position pos) {
+      return Token(TokenType::SquareOpen, "", pos);
+    }
+
+    static Token SquareClose(position pos) {
+      return Token(TokenType::SquareClose, "", pos);
+    }
+
+    static Token CurlyOpen(position pos) {
+      return Token(TokenType::CurlyOpen, "", pos);
+    }
+
+    static Token CurlyClose(position pos) {
+      return Token(TokenType::CurlyClose, "", pos);
+    }
+
+    static Token SetOpen(position pos) {
+      return Token(TokenType::SetOpen, "", pos);
+    }
+
+    static Token FunctionOpen(position pos) {
+      return Token(TokenType::FunctionOpen, "", pos);
+    }
+
+    static Token Dispatch(std::string value, position pos) {
+      return Token(TokenType::Dispatch, value, pos);
+    }
+
+    static Token String(std::string value, position pos) {
+      return Token(TokenType::String, value, pos);
+    }
+
+    static Token Regex(std::string value, position pos) {
+      return Token(TokenType::Regex, value, pos);
+    }
+
+    static Token Char(std::string value, position pos) {
+      return Token(TokenType::Char, value, pos);
+    }
+
+  private:
+    Token(TokenType type, std::string value, position pos)
+        : type(type), value(value), pos(pos) { }
+  };
+
+  inline ::std::ostream &operator<<(::std::ostream &os, const Token &token) {
+    return os << token.DebugInfo();
   }
-
-  bool operator==(const Token other) const;
-
-  TokenType type;
-  std::string value;
-  position pos;
-
-  static Token None() {
-    return Token(TokenType::None, "", std::make_tuple(-1,-1));
-  }
-
-  static Token Literal(std::string value, position pos) {
-    return Token(TokenType::Literal, value, pos);
-  }
-
-  static Token RoundOpen(position pos) {
-    return Token(TokenType::RoundOpen, "", pos);
-  }
-
-  static Token RoundClose(position pos) {
-    return Token(TokenType::RoundClose, "", pos);
-  }
-
-  static Token SquareOpen(position pos) {
-    return Token(TokenType::SquareOpen, "", pos);
-  }
-
-  static Token SquareClose(position pos) {
-    return Token(TokenType::SquareClose, "", pos);
-  }
-
-  static Token CurlyOpen(position pos) {
-    return Token(TokenType::CurlyOpen, "", pos);
-  }
-
-  static Token CurlyClose(position pos) {
-    return Token(TokenType::CurlyClose, "", pos);
-  }
-
-  static Token SetOpen(position pos) {
-    return Token(TokenType::SetOpen, "", pos);
-  }
-
-  static Token FunctionOpen(position pos) {
-    return Token(TokenType::FunctionOpen, "", pos);
-  }
-
-  static Token Dispatch(std::string value, position pos) {
-    return Token(TokenType::Dispatch, value, pos);
-  }
-
-  static Token String(std::string value, position pos) {
-    return Token(TokenType::String, value, pos);
-  }
-
-  static Token Regex(std::string value, position pos) {
-    return Token(TokenType::Regex, value, pos);
-  }
-
-  static Token Char(std::string value, position pos) {
-    return Token(TokenType::Char, value, pos);
-  }
-
-private:
-  Token(TokenType type, std::string value, position pos)
-      : type(type), value(value), pos(pos) {}
-};
-
-inline ::std::ostream& operator<<(::std::ostream& os, const Token &token) {
-  return os << token.DebugInfo();
 }
 
+using namespace token;
+
 class Tokenizer {
+
 public:
 
   Tokenizer(std::unique_ptr<Scanner> &s) :
@@ -207,7 +213,7 @@ private:
   bool ready = false;
 
   std::unique_ptr<Scanner> scanner;
-  Token m_end = Token::None();
+  Token m_end = Token::EndOfFile();
   Token current = m_end;
   Token waiting = m_end;
 };
